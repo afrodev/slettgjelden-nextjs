33a0824364e23e5a9e018f1fe0fcfbdd3eb49840
BlocBoiMonzer <monzergmc1@gmail.com>
2023-11-04 20:21:18 +0100

    functioning input outputs and simple component
    The app works for I/O and has some basic tailwind css

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,14 @@
-const InputField = ({ name }) => {
+interface InputFieldProps {
+    name: string;
+    value: any;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ name, value, onChange }) => {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between w-full">
             <label>{name}</label>
-            <input type="text" name={name} className="w-full max-w-[9rem] "/>
+            <input type="text" name={name} value={value} onChange={onChange} className="w-full max-w-[9rem] "/>
         </div>
     );
 }
